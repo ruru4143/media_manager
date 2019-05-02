5782e43c004bc2f1567e0ae066ba206b05b7c8fc
ruru4143 <ruru@r3.at>
2019-05-02 10:00:57 +0200

    add bash script

---
+++ media-manager.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+path=$1
+
+echo "Welcome to the show, rename, delete media manager"
+
+image_extensions="jpeg png jpg pnm tiff bmp mov"
+video_extensions="m4a 3gp 3g2 mj2 gif mp4"
+
+for file in $(find "$path" -maxdepth 1 -type f | egrep "(\.jpg)|(\.jpeg)|(\.png)|(\.pnm)|(\.tiff)|(\.bmp)|(\.mov)|(\.mp4)|(\.m4a)|(\.3gp)|(\.3g2)|(\.mj2)|(\.gif)"); do  
+  if [[ $image_extensions == *"${file#*.}"* ]]; then
+    feh --full-screen $file
+  elif [[ $video_extensions == *"${file#*.}"* ]]; then
+    mpv --fullscreen $file
+  else
+    echo "failing..."
+  fi
+  
+  echo $file
+  echo -n "do you want to do n(othing), d(elete), r(ename): "
+  read operator
+  
+  if [[ "$operator" == "d" ]]; then
+    rm $file
+  elif [[ "$operator" == "r" ]]; then
+    echo -n "                                                 		new name: "
+    read new_name
+	mv $file $(echo $path/$new_name\.${file#*.})
+  else
+    echo "do nothing"
+  fi
+  
+done
